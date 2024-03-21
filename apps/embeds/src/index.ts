6589879d7e52852a65d11a6e2c3aeb797006297b
thom-10 <thomaszehnder@outlook.de>
2024-03-21 23:52:52 +0100

    remove next

---
+++ apps/embeds/src/index.ts
@@ -0,0 +1,20 @@
+const init = () => {
+  const scripts = document.querySelector('script#gw2embeds');
+  if (scripts == null) {
+    import('./App')
+      .then(({ default: App }) => App())
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+};
+
+export default (function setGW2Embed() {
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', () => {
+      init();
+    });
+  } else {
+    setTimeout(init, 1);
+  }
+})();
