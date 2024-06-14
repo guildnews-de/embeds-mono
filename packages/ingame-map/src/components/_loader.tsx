08050724efa620c0a568b3ee2f5f23525cfed74a
thom-10 <thomaszehnder@outlook.de>
2024-06-15 00:07:54 +0200

    add api request

--- packages/ingame-map/src/components/_loader.tsx
+++ packages/ingame-map/src/components/_loader.tsx
@@ -9,6 +9,8 @@ import type { MapContComp } from './MapCont';
 import type { MarkerButtonComp } from './MarkerButton';
 
 import '../package.scss';
+import { useMemo } from 'react';
+// import { useEffect } from 'react';
 
 type IngameMapComponent = MapContComp | MarkerButtonComp;
 
@@ -18,7 +20,33 @@ export interface IngameMapLoaderProps extends Omit<IngameMapProps, 'data'> {
 
 export default function IngameMapLoader(props: IngameMapLoaderProps) {
   const { data, actions, hooks, hash } = props;
-  const elementData = new IngameMapData(data);
+  const { addActiveMap, setMapsLoaded, fetchMap } = actions;
+  const { useAppDispatch /* , useAppSelector */ } = hooks;
+  const dispatch = useAppDispatch();
+  // const { mapsLoaded } = useAppSelector((state) => state.app);
+  // const elementData = new IngameMapData(data);
+  const elementData = useMemo(() => {
+    return new IngameMapData(data);
+  }, [data]);
+
+  useMemo(() => {
+    const { ids } = elementData;
+    if (ids && ids.length > 0 /* && !mapsLoaded */) {
+      ids.forEach((id) => {
+        // const numID = Number(id);
+        dispatch(fetchMap({ id: id, lang: 'de' }));
+        dispatch(addActiveMap(id));
+      });
+      dispatch(setMapsLoaded());
+    }
+  }, [
+    dispatch,
+    fetchMap,
+    addActiveMap,
+    setMapsLoaded,
+    // mapsLoaded,
+    elementData,
+  ]);
 
   const AsyncModule = loadable<IngameMapProps>(
     ({ data }) => import(`./${data.type}`) as Promise<IngameMapComponent>,
