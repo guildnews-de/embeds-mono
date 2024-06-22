78acca6fa21481ee7af6d85794edc9a9011d25d6
thom-10 <thomaszehnder@outlook.de>
2024-06-22 12:35:11 +0200

    delete unused styling

--- packages/timer/src/components/MetaPhase.tsx
+++ packages/timer/src/components/MetaPhase.tsx
@@ -6,21 +6,17 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
   margin: theme.spacing(0.25),
   padding: theme.spacing(0.5),
   flexBasis: 'min-content',
-  overflow: '',
 }));
 
 export default function MetaPhase({ phase }: { phase: TimerSegment }) {
   const { color, duration, name } = phase;
-  const maxWidth = 1.2 * duration;
   return (
     <StyledPaper
       className="phase"
       sx={{
         backgroundColor: color,
         flexGrow: duration,
-        maxWidth: `${maxWidth}%`,
       }}
-      // flexGrow={duration}
       // style$="background: [[ phase.color ]]; color: [[ _textColor(phase.text) ]]; width:calc([[ _calcPhaseWidth(phase.duration) ]]% - .25rem);"
     >
       <Box className="phase-time" sx={{ minWidth: 0 }}>
