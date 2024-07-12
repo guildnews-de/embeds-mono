736c5bec5702e1bd962d1f3d63af11a199a011a5
thom-10 <thomaszehnder@outlook.de>
2024-07-13 00:28:49 +0200

    adjust fonts

--- apps/embeds/src/shared/theme.ts
+++ apps/embeds/src/shared/theme.ts
@@ -2,7 +2,15 @@ import { createTheme, type Theme } from '@mui/material';
 
 export const embedTheme: Theme = createTheme({
   typography: {
-    fontFamily:
-      'Menomonia, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    fontFamily: [
+      'Menomonia',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      'Helvetica',
+      'Arial',
+      'sans-serif',
+    ].join(','),
   },
 });
