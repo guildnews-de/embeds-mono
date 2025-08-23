a641c32265e71acbfd72088fa84ba4c78c7df01f
thom-10 <thomaszehnder@outlook.de>
2025-08-24 01:44:30 +0200

    remove webpack

--- config/webpack-config/webpack.app.js
+++
@@ -1,9 +0,0 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
-
-module.exports = merge(common, {
-  target: 'web',
-  output: {
-    filename: 'index.js',
-  },
-});
