350ffba9ee9471b1b63bce8821c02b08acec1941
thom-10 <thomaszehnder@outlook.de>
2025-12-01 20:18:20 +0100

    swtich to native defineConfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,11 +5,11 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
-import { globalIgnores } from 'eslint/config';
+import { globalIgnores, defineConfig } from 'eslint/config';
 
 /** @type { import("typescript-eslint").Config } */
-const config = tseslint.config([
-  globalIgnores(['dist/*']),
+const config = defineConfig([
+  globalIgnores(['dist/*', '**/vendor/*']),
   {
     ignores: ['.prettierrc.js', 'eslint.config.mjs'],
   },
